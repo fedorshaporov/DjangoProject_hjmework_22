5cefa8353c2395d5e0f5dc0737dde3f782f33705
Fedor Shaporov <shaporovfed@gmail.com>
2026-04-03 16:28:22 +0300

    перевел FBV на CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_detail
-
+from catalog.views import HomeView, ContactsView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:product_id>/', product_detail, name='product_detail')
-
+    path('', HomeView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
 ]
