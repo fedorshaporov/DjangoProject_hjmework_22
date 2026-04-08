ea5861bdad65721b8a7925e4c95bf380af94c3e7
Fedor Shaporov <shaporovfed@gmail.com>
2026-04-08 21:49:56 +0300

    добавил forms.py и  сделал сделал валидацию и стилизацию

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
+from django.contrib import messages
 from catalog.models import Product
+from catalog.forms import ProductForm  # Импортируйте созданную вами форму
+from django.core.exceptions import ValidationError
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -15,6 +18,56 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
     context_object_name = 'product'
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'  # Убедитесь, что этот шаблон существует
+    context_object_name = 'products'
+
+
+class ProductCreateView(View):
+    template_name = 'product_form.html'
+
+    def get(self, request, *args, **kwargs):
+        form = ProductForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:product_list')  # Убедитесь, что 'product_list' правильно ссылается на маршрут
+        return render(request, self.template_name, {'form': form})
+
+
+class ProductUpdateView(View):
+    template_name = 'product_form.html'
+
+    def get(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        form = ProductForm(instance=product)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:product_list')  # Убедитесь, что это правильно
+        return render(request, self.template_name, {'form': form})
+
+class ProductDeleteView(View):
+    template_name = 'product_confirm_delete.html'
+
+    def get(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        return render(request, self.template_name, {'product': product})
+
+    def post(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        messages.success(request, 'Продукт успешно удален.')
+        return redirect('product_list')
 
 class ContactsView(View):
     template_name = 'contacts.html'
